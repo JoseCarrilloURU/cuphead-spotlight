febc7182f6f4f7b376fc5c003b637a0c73e4f2a3
padallain <123842369+padallain@users.noreply.github.com>
2024-11-28 22:08:43 -0400

    ready

--- app/(tabs)/discover.tsx
+++ app/(tabs)/discover.tsx
@@ -62,6 +62,7 @@ export default function Home() {
   useEffect(() => {
     if (!searchPersonId) {
       console.log("personId is not available yet");
+      console.log("hello");
       return;
     }
 
